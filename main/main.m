14b2e07c9586eb31c093fbcd6dad58195dc97916
ChangYeongHan <112463975+ChangYeongHan@users.noreply.github.com>
2025-01-17 16:48:18 +0900

    Update main.m

--- main/main.m
+++ main/main.m
@@ -3,27 +3,6 @@ clear
 close all
 clc
 
-set(0,'DefaultAxesFontName','Arial');
-
-fs_label = 20;
-fs_tick_small = 15;
-fs_tick_medium = 18;
-fs_legend = 15;
-fs_title = 22; 
-
-%% Plotting for all Results
-
-% Draw the result at Experiment 1 & 2
-Color_Darkgray = [25 25 25]/255;
-Color_Gray = [100 100 100]/255;
-
-Color_Yellow = [245 213 71]/255; %% yellow
-Color_Orange = [237 125 49]/255; %% orange
-
-Color_Navy = [20 70 160]/255; %% navy
-Color_SkyBlue = [90 180 220]/255; %% sky blue
-
-color_yb = [44, 102, 107; 101, 173, 178; 215, 149, 79; 236, 186, 101; 240, 214, 98]/255;
 Hz2s= [120 144 160 180 240 288];
 
 %% Make Stimulus
@@ -44,8 +23,8 @@ gau_test = linspace(gau_LB(1),gau_UB(1),50);
 %% Fitting the Result: Experiment 1 and 2
 load('Exp12.mat')
 
-params0 = [120, 11, 10, 10];
-params1 = [0.0027, 15.2459, 1.9814, 10.5 0.2];
+params0 = [120, 11, 10, 10]; % parameters for Gaussian filter (Nakajima & Sakaguthi, 2015)
+params1 = [0.0027, 15.2459, 1.9814, 10.5 0.2]; % parameters for bi-phasic filter (fitted in TCSF at 8300 td in Kelly, 1961)
 options = optimset('MaxIter', 1e+5,'MaxFunEvals', 1e+5);
 
 
@@ -58,8 +37,8 @@ for i = 1:size(conThresExp0,1)
         bi_test_temp = [bi_test_lum(l) 0.5 0.1];
         gau_test_temp = [gau_test(l) 0.5 0.1];
 
-        best_params_gau1_temp(l,:) = fminsearchbnd( @(params) work_vrr_moving_average(params, data1), gau_test_temp,gau_LB,gau_UB, options);
-        mse0_gau_temp(l) = work_vrr_moving_average(best_params_gau1_temp(l,:), data1);
+        best_params_gau1_temp(l,:) = fminsearchbnd( @(params) work_vrr_gaussian(params, data1), gau_test_temp,gau_LB,gau_UB, options);
+        mse0_gau_temp(l) = work_vrr_gaussian(best_params_gau1_temp(l,:), data1);
         
         best_params_bi1_temp(l,:) = fminsearchbnd( @(params) work_vrr_biphasic(params,data1), bi_test_temp, bi_LB, bi_UB1, options);
         mse0_bi_temp(l) = work_vrr_biphasic(best_params_bi1_temp(l,:), data1);
@@ -67,25 +46,23 @@ for i = 1:size(conThresExp0,1)
     
     lowest_idx_gau = find(mse0_gau_temp == min(mse0_gau_temp),1,'first');
     best_params_gau1(i,:) = best_params_gau1_temp(lowest_idx_gau,:);
-    [mse0_gau(i), conThresh_Moving0(i,:), gau_irf1{i}] = work_vrr_moving_average(best_params_gau1(i,:), data1);
+    [mse0_gau(i), conThresh_gaussian0(i,:), gau_irf1{i}] = work_vrr_gaussian(best_params_gau1(i,:), data1);
 
     lowest_idx_bi = find(mse0_bi_temp == min(mse0_bi_temp),1,'first');
     best_params_bi1(i,:) = best_params_bi1_temp(lowest_idx_bi,:);
-    [mse0_bi(i), conThresh_UNIST0(i,:), bi_irf1(i,:)] = work_vrr_biphasic(best_params_bi1(i,:), data1);
+    [mse0_bi(i), conThresh_biphasic0(i,:), bi_irf1(i,:)] = work_vrr_biphasic(best_params_bi1(i,:), data1);
 
 end
 
-conThresh_Moving1 = conThresh_Moving0(:,1:12);
-conThresh_Moving2 = conThresh_Moving0(:,13:24);
-conThresh_UNIST1 = conThresh_UNIST0(:,1:12);
-conThresh_UNIST2 = conThresh_UNIST0(:,13:24);
+conThresh_gaussian1 = conThresh_gaussian0(:,1:12);
+conThresh_gaussian2 = conThresh_gaussian0(:,13:24);
+conThresh_biphasic1 = conThresh_biphasic0(:,1:12);
+conThresh_biphasic2 = conThresh_biphasic0(:,13:24);
 
 
 %% Fitting the Results: Experiment 3
 
 load('Exp3.mat')
-sub = 1:11;
-thres_Exp3 = thres_Exp3(sub,:);
 
 for i = 1:11
     data3.params = params1(1:3);
@@ -96,23 +73,23 @@ for i = 1:11
         bi_test_temp = [bi_test_lum(l) 0.5 0.1];
         gau_test_temp = [gau_test(l) 0.5 0.1];
 
-        best_params_gau2_temp(l,:) = fminsearchbnd( @(params) work_vrr_moving_average(params, data3),gau_test_temp,gau_LB,gau_UB, options);
-        mse2_gau_temp(l) = work_vrr_moving_average(best_params_gau2_temp(l,:), data3);
+        best_params_gau2_temp(l,:) = fminsearchbnd( @(params) work_vrr_gaussian(params, data3),gau_test_temp,gau_LB,gau_UB, options);
+        mse2_gau_temp(l) = work_vrr_gaussian(best_params_gau2_temp(l,:), data3);
         
         best_params_bi2_temp(l,:) = fminsearchbnd( @(params) work_vrr_biphasic(params,data3), bi_test_temp, bi_LB, bi_UB1, options);
         mse2_bi_temp(l) = work_vrr_biphasic(best_params_bi2_temp(l,:), data3);
     end
     lowest_idx_gau = find(mse2_gau_temp == min(mse2_gau_temp),1,'first');
     best_params_gau2(i,:) = best_params_gau2_temp(lowest_idx_gau,:);
-    [mse2_gau(i), conThresh_Moving3(i,:), gau_irf2{i}] = work_vrr_moving_average(best_params_gau2(i,:), data3);
+    [mse2_gau(i), conThresh_gaussian3(i,:), gau_irf2{i}] = work_vrr_gaussian(best_params_gau2(i,:), data3);
     lowest_idx_bi = find(mse2_bi_temp == min(mse2_bi_temp),1,'first');
     best_params_bi2(i,:) = best_params_bi2_temp(lowest_idx_bi,:);
-    [mse2_bi(i), conThresh_UNIST3(i,:), bi_irf2(i,:)] = work_vrr_biphasic(best_params_bi2(i,:), data3);
+    [mse2_bi(i), conThresh_biphasic3(i,:), bi_irf2(i,:)] = work_vrr_biphasic(best_params_bi2(i,:), data3);
 
 end
 
-conThreshFreq3_gau = mean(conThresh_Moving3);
-conThreshFreq3_bi = mean(conThresh_UNIST3);
+conThreshFreq3_gau = mean(conThresh_gaussian3);
+conThreshFreq3_bi = mean(conThresh_biphasic3);
 
 
 %% Fitting the results: Experiment 4
@@ -131,21 +108,21 @@ for i = 1:size(thres_Exp4,1)
         bi_test_temp = [bi_test_color(l) 0.5 0.1];
         gau_test_temp = [gau_test(l) 0.5 0.1];
 
-        best_params_gau3_temp(l,:) = fminsearchbnd( @(params) work_vrr_moving_average(params, data4),gau_test_temp,gau_LB,gau_UB, options);
-        mse3_gau_temp(l)= work_vrr_moving_average(best_params_gau3_temp(l,:), data4);
+        best_params_gau3_temp(l,:) = fminsearchbnd( @(params) work_vrr_gaussian(params, data4),gau_test_temp,gau_LB,gau_UB, options);
+        mse3_gau_temp(l)= work_vrr_gaussian(best_params_gau3_temp(l,:), data4);
         
         best_params_bi3_temp(l,:) = fminsearchbnd( @(params) work_vrr_biphasic(params,data4), bi_test_temp, bi_LB, bi_UB2, options);
         mse3_bi_temp(l) = work_vrr_biphasic(best_params_bi3_temp(l,:), data4);
     end
     lowest_idx_gau = find(mse3_gau_temp == min(mse3_gau_temp),1,'first');
     best_params_gau3(i,:) = best_params_gau3_temp(lowest_idx_gau,:);
-    [mse3_gau(i), conThresh_Moving4(i,:), gau_irf3{i}] = work_vrr_moving_average(best_params_gau3(i,:), data4);
+    [mse3_gau(i), conThresh_gaussian4(i,:), gau_irf3{i}] = work_vrr_gaussian(best_params_gau3(i,:), data4);
 
     lowest_idx_bi = find(mse3_bi_temp == min(mse3_bi_temp),1,'first');
     best_params_bi3(i,:) = best_params_bi3_temp(lowest_idx_bi,:);
-    [mse3_bi(i), conThresh_UNIST4(i,:), bi_irf3(i,:)] = work_vrr_biphasic(best_params_bi3(i,:), data4);
+    [mse3_bi(i), conThresh_biphasic4(i,:), bi_irf3(i,:)] = work_vrr_biphasic(best_params_bi3(i,:), data4);
 
 end
 
-conThreshFreq4_gau = mean(conThresh_Moving4);
-conThreshFreq4_bi = mean(conThresh_UNIST4);
+conThreshFreq4_gau = mean(conThresh_gaussian4);
+conThreshFreq4_bi = mean(conThresh_biphasic4);
